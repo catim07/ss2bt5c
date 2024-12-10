b0e7f98efbfb6c0cf6604f1bd9994a5831a30dfd
catim07 <cuongdfle@gmail.com>
2024-12-10 08:45:35 +0700

    Add files via upload

---
+++ ss2bt5.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int dai = 5;
+    int rong = 10;
+    int  chuvi = 2 *(dai + rong);
+    int dientich = dai * rong;
+    printf("chieu dai hinh chu nhat: %d\n", dai);
+    printf("chieu rong hinh chu nhat: %d\n",rong);
+    printf("Chu vi hinh chu nhat: %d\n", chuvi);
+    printf("Dien tich hinh chu nhat: %d\n", dientich);
+
+    return 0;
+}
+
